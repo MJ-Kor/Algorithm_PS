776729e27ea09916ba145afce79845d3fb00d165
MJ-Kor <87603324+MJ-Kor@users.noreply.github.com>
2024-01-23 01:11:15 +0900

    [Silver V] Title: 다리 놓기, Time: 264 ms, Memory: 18540 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+ 
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		int[][] dp = new int[30][30];
+ 
+		for (int i = 0; i < 30; i++) {
+			dp[i][i] = 1;
+			dp[i][0] = 1;
+		}
+			
+ 
+		for (int i = 2; i < 30; i++) {
+			for (int j = 1; j < 30; j++) { 
+				dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+			}
+		}
+        
+        
+		
+		int T = in.nextInt();
+	
+		StringBuilder sb = new StringBuilder();
+        
+		for(int i = 0; i < T; i++) {
+ 
+			int N = in.nextInt();	
+			int M = in.nextInt();	
+			
+			sb.append(dp[M][N]).append('\n');
+		}
+		
+		System.out.println(sb);
+ 
+	}
+}
