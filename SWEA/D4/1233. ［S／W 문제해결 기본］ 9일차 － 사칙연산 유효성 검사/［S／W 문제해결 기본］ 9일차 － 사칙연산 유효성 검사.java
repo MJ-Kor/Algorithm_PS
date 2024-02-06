d3cc2a27a2b0d0eb4568d228fc2fa9035efcb293
MJ-Kor <87603324+MJ-Kor@users.noreply.github.com>
2024-02-06 12:04:33 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 사칙연산 유효성 검사, Time: 123 ms, Memory: 21,900 KB -BaekjoonHub

---
+++ SWEA/D4/1233. ［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사/［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사.java
@@ -0,0 +1,35 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		int T = 10;
+		int N;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		for (int i = 1; i <= T; i++) {
+			N = Integer.parseInt(br.readLine());
+			int valid = 1;
+			for (int j = 0; j < N; j++) {
+				String[] nodeInfo = br.readLine().split(" ");
+				if(Character.isDigit(nodeInfo[1].charAt(0))) {
+					if(nodeInfo.length > 2) {
+						valid = 0;
+					}
+				}
+				else {
+					if(nodeInfo.length != 4) {
+						valid = 0;
+					}
+				}
+			}
+			
+			System.out.print("#" + i + " " + valid);
+			System.out.println();
+		
+		}
+
+	}
+}
