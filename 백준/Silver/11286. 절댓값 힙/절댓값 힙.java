e7535400110c60c4e4034f99a6ba360cd47d7615
MJ-Kor <87603324+MJ-Kor@users.noreply.github.com>
2024-02-07 11:38:08 +0900

    [Silver I] Title: 절댓값 힙, Time: 652 ms, Memory: 26304 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Main {
+		
+	private static PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+
+		@Override
+		public int compare(Integer o1, Integer o2) {
+			if(Math.abs(o1) == Math.abs(o2)) {
+				return o1.compareTo(o2);
+			}
+			return Math.abs(o1) - Math.abs(o2);
+		}
+	});
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int input;
+		for (int i = 0; i < N; i++) {
+			input = Integer.parseInt(br.readLine());
+			if(input != 0) {
+				pq.add(input);
+			}
+			else {
+				if(pq.isEmpty()) {
+					System.out.println(0);
+				}
+				else {
+					System.out.println(pq.poll());
+				}
+			}
+		}
+	}
+}
