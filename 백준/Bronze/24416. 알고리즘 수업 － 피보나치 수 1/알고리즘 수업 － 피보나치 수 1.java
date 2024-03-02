65384d79956bae23a2e266e94961aba1de125799
MJ-Kor <87603324+MJ-Kor@users.noreply.github.com>
2024-03-03 03:02:37 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 460 ms, Memory: 16124 KB -BaekjoonHub

---
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		int num = fib(N);
+		System.out.println(num + " " + (N - 2));
+	}
+
+	private static int fib(int n) {
+		
+		if(n == 1 || n == 2) {
+			return 1;
+		}
+		else return(fib(n - 1) + fib(n - 2));	
+	}
+}
