75031b5685b258c28843b2113130a8847e58f4eb
MJ-Kor <87603324+MJ-Kor@users.noreply.github.com>
2024-03-04 01:09:36 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 136 ms, Memory: 14500 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -1,110 +1,46 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	private static int N, ans;
-	private static int[][] map;
-	private static int[] vertical = {1, 0};
-	private static int[] horizontal = {0, 1};
-	private static int[] diagonal = {1, 1};
-
-			
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+		StringTokenizer st = null;
 		
-		N = Integer.parseInt(br.readLine());
-		map = new int[N + 1][N + 1];
+		int N = Integer.parseInt(br.readLine());
+		int[][] map = new int[N][N];
+		int[][][] dp = new int[N][N][3];
+		dp[0][1][0] = 1;
 		
-		for (int r = 1; r <= N; r++) {
-			st = new StringTokenizer(br.readLine());
-			for (int c = 1; c <= N; c++) {
-				map[r][c] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		movePipe(1, 2, "horizontal");
-		System.out.println(ans);
-	}
-	
-	public static void movePipe(int r, int c, String pipeState) {
-		
-		int nr, nc;
-		
-		if(r == N && c == N) {
-			ans++;
-			return;
+		for (int r = 0; r < N; r++) {
+			map[r] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 		}
 		
-		if(pipeState.equals("vertical")) {
-			
-			if(verticalCheck(r + vertical[0], c + vertical[1])) {
-				nr = r + vertical[0];
-				nc = c + vertical[1];
-				movePipe(nr, nc, "vertical");
-			}
-			if(diagonalCheck(r + vertical[0], c + vertical[1], r + horizontal[0], c + horizontal[1], r + diagonal[0], c + diagonal[1])) {
-				nr = r + diagonal[0];
-				nc = c + diagonal[1];
-				movePipe(nr, nc, "diagonal");
-			}
-		}
-		else if(pipeState.equals("horizontal")) {
-			
-			if(horizontalCheck(r + horizontal[0], c + horizontal[1])) {
-				nr = r + horizontal[0];
-				nc = c + horizontal[1];
-				movePipe(nr, nc, "horizontal");
-			}
-			if(diagonalCheck(r + vertical[0], c + vertical[1], r + horizontal[0], c + horizontal[1], r + diagonal[0], c + diagonal[1])) {
-				nr = r + diagonal[0];
-				nc = c + diagonal[1];
-				movePipe(nr, nc, "diagonal");
+		for (int r = 0; r < N; r++) {
+			for (int c = 2; c < N; c++) {
+				
+				if(map[r][c] == 1) continue;
+				
+				dp[r][c][0] = dp[r][c - 1][0] + dp[r][c - 1][2];
+				
+				if(r == 0) {
+					continue;
+				}
+				
+				dp[r][c][1] = dp[r - 1][c][1] + dp[r - 1][c][2];
+				
+				if(map[r - 1][c] == 1 || map[r][c - 1] == 1) {
+					continue;
+				}
+				
+				dp[r][c][2] = dp[r - 1][c - 1][0] + dp[r - 1][c - 1][1] + dp[r - 1][c - 1][2];
 			}
 		}
-		else if(pipeState.equals("diagonal")) {
-			
-			if(horizontalCheck(r + horizontal[0], c + horizontal[1])) {
-				nr = r + horizontal[0];
-				nc = c + horizontal[1];
-				movePipe(nr, nc, "horizontal");
-			}
-			if(diagonalCheck(r + vertical[0], c + vertical[1], r + horizontal[0], c + horizontal[1], r + diagonal[0], c + diagonal[1])) {
-				nr = r + diagonal[0];
-				nc = c + diagonal[1];
-				movePipe(nr, nc, "diagonal");
-			}
-			if(verticalCheck(r + vertical[0], c + vertical[1])) {
-				nr = r + vertical[0];
-				nc = c + vertical[1];
-				movePipe(nr, nc, "vertical");
-			}
-		}
-	}
-	
-	public static boolean verticalCheck(int nr, int nc) {
-		if(nr >= 1 && nr <= N && nc >= 1 && nc <= N && map[nr][nc] != 1) {
-			return true;
-		}
-		return false;
-	}
-	
-	public static boolean horizontalCheck(int nr, int nc) {
-		if(nr >= 1 && nr <= N && nc >= 1 && nc <= N && map[nr][nc] != 1) {
-			return true;
-		}
-		return false;
-	}
-	
-	public static boolean diagonalCheck(int v_nr, int v_nc, int h_nr, int h_nc, int d_nr, int d_nc) {
-		if(verticalCheck(v_nr, v_nc) && horizontalCheck(h_nr, h_nc) && d_nr >= 1 && d_nr <= N && d_nc >= 1 && d_nc <= N && map[d_nr][d_nc] != 1) {
-			return true;
-		}
-		return false;
+		
+		System.out.println(dp[N - 1][N - 1][0] + dp[N - 1][N - 1][1] + dp[N - 1][N - 1][2]);
 	}
 
 }
