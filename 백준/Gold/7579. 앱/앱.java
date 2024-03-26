a3966467f12f73869e5f3ad66f230637f47f853f
MJ-Kor <87603324+MJ-Kor@users.noreply.github.com>
2024-03-26 23:05:55 +0900

    [Gold III] Title: 앱, Time: 200 ms, Memory: 21748 KB -BaekjoonHub

---
+++ 백준/Gold/7579. 앱/앱.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int sum = Integer.MAX_VALUE;
+		
+		int[] m = new int[N];
+		int[] c = new int[N];
+		int[][] dp = new int[N][10001];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			m[i] = Integer.parseInt(st.nextToken());
+		}
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			c[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		
+		for (int i = 0; i < N; i++) {
+			int cost = c[i];
+			int memory = m[i];
+			
+			for (int j = 0; j <= 10000; j++) {
+				if(i == 0) {
+					if(j >= cost) {
+						dp[i][j] = memory;
+					}
+				} 
+				else {
+					if(j >= cost) {
+						dp[i][j] = Math.max(dp[i - 1][j - cost] + memory, dp[i - 1][j]);
+					} else {
+						dp[i][j] = dp[i - 1][j];	
+					}
+				}
+				
+				if(dp[i][j] >= M) {
+					sum = Math.min(sum, j);
+				}
+			}
+		}
+		
+		System.out.println(sum);
+	}
+
+}
