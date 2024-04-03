d0a4724cf462908f48d96b8f15ee6b7b1b8265f2
김민주 <87603324+MJ-Kor@users.noreply.github.com>
2024-04-03 11:36:34 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 540 ms, Memory: 96280 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	private static int[] dp;
+	private static int[] arr;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		int N = Integer.parseInt(br.readLine());
+		arr = new int[N];
+		dp = new int[N + 1];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int idx = 0;
+		int len = 0;
+		for (int i = 0; i < N; i++) {
+			if(arr[i] > dp[len]) {
+				len++;
+				dp[len] = arr[i];
+			}
+			else {
+				idx = binarySearchLIS(arr[i], 0, len);
+				dp[idx] = arr[i];
+			}
+		}
+		System.out.println(len);
+	}
+
+	private static int binarySearchLIS(int value, int start, int end) {
+		if(start > end) return start;
+		
+		int mid = (start + end) / 2;
+		
+		if(dp[mid] < value) return binarySearchLIS(value, mid + 1, end);
+		else return binarySearchLIS(value, start, mid - 1);
+	}
+
+}
