e31671619bec7705762dea5f3907d7ab82467ce9
김민주 <87603324+MJ-Kor@users.noreply.github.com>
2024-07-14 02:03:48 +0900

    [Silver II] Title: A → B, Time: 104 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Silver/16953. A → B/A → B.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        String A = input[0];
+        String B = input[1];
+        int cnt = 0;
+
+        while (true) {
+            if (B.endsWith("1")) {
+                B = B.substring(0, B.length() - 1);
+                cnt++;
+            } else if (Integer.parseInt(B) % 2 == 0) {
+                B = String.valueOf(Integer.parseInt(B) / 2);
+                cnt++;
+            } else if (Integer.parseInt(B) % 2 == 1) {
+                if (!B.equals(A)) {
+                    cnt = -1;
+                    break;
+                }
+            }
+
+            if (Integer.parseInt(B) < Integer.parseInt(A) || B.equals("")) {
+                cnt = -1;
+                break;
+            }
+
+            if (B.equals(A)) {
+                cnt++;
+                break;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
